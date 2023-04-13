26da79eb3ee35c203afaae39dc893f7171ccde90
caro1503 <c.agudelo@utp.edu.co>
2023-04-13 07:25:16 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavigationModule } from './core/components/navigation/navigation.module';
+import { APP_BASE_HREF } from '@angular/common';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent, ToolbarComponent
+  ], 
   imports: [
-    BrowserModule
+    BrowserModule,
+    NavigationModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
